c0c60e024eaf3a3a96b901688e2dbde4b592a33b
rimca <eguducasidi0@gmail.com>
2023-08-23 11:03:45 +0000

    NodeJS Basics & Unittests in JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Casmir  <https://github.com/rimcas>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
